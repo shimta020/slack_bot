e98e56f31cfb629a0e9fd7b2ca84f12e76887f79
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-03-05 11:15:32 +0900

    clockeork導入

---
+++ source_cord_reading/sample.rb
@@ -0,0 +1,19 @@
+require "slack-notify"
+require 'clockwork'
+require 'active_support/time'
+require 'dotenv'
+Dotenv.load
+
+module Clockwork
+  client = SlackNotify::Client.new(
+    webhook_url: ENV['slack_url'],
+    channel: "#general",
+    username: "incoming-webhook",
+    icon_url: "http://mydomain.com/myimage.png",
+    icon_emoji: ":shipit:",
+    link_names: 1
+  )
+  every(5.seconds, 'notify') do
+    client.notify("Hello There!")
+  end
+end
